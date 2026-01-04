4fb7b5b0d8c4d94afcf94163861d71caa15f12f0
John Ling <johnlingbusiness@gmail.com>
2026-01-04 12:03:45 +1100

    refactor: prettified api response processing

--- src/plugins/widgets/trello/utils/api.ts
+++ src/plugins/widgets/trello/utils/api.ts
@@ -8,7 +8,12 @@ export const getBoards = async (session: TrelloSession) => {
     }
 
     const boardData: TrelloBoardResponse[] = await fetchBoardRes.json();
-    const boards: Board[] = boardData.map((data: TrelloBoardResponse) => ({ id: data.id, name: data.name } as Board));
+    const boards: Board[] = boardData.map((data: TrelloBoardResponse) => 
+        ({ 
+            id: data.id, 
+            name: data.name 
+        } as Board)
+    );
     return boards;
 }
 
@@ -20,7 +25,13 @@ export const getLists = async (boardId: string, session: TrelloSession) => {
     }
 
     const listData: TrelloListResponse[] = await fetchListRes.json();
-    const lists: List[] = listData.map((data: TrelloListResponse) => ({ id: data.id, name: data.name, boardID: boardId, watch: false } as List));
+    const lists: List[] = listData.map((data: TrelloListResponse) => 
+        ({ 
+            id: data.id, 
+            name: data.name, 
+            boardID: boardId, 
+            watch: false 
+        } as List));
     return lists;
 }
 
@@ -32,6 +43,11 @@ export const getItems = async (listId: string, session: TrelloSession) => {
     }
 
     const data: TrelloItemsResponse = await fetchItemsRes.json();
-    const items = data.map((item: TrelloListItem) => ({ id: item.id, name: item.name, labels: item.labels } as Item));
+    const items = data.map((item: TrelloListItem) => 
+        ({ 
+            id: item.id, 
+            name: item.name, 
+            labels: item.labels 
+        } as Item));
     return items;
 }
