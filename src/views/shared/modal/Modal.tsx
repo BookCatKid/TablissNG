e7ae76a9196bdf4c4a226ed5ff1e24e1fb784e8b
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-02-06 19:21:28 -0800

    refactor: improve Modal component and IconPicker UI
    Update the shared Modal component to support footers, custom classes,
    and centering. Refactor the IconPickerModal to use this shared Modal
    component instead of a custom implementation. Additionally, update
    associated styles to ensure consistent look and feel with the rest of
    the application and improve dark mode support.

--- src/views/shared/modal/Modal.tsx
+++ src/views/shared/modal/Modal.tsx
@@ -4,14 +4,24 @@ import type { FC, ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
+  footer?: ReactNode;
   onClose: () => void;
+  className?: string;
+  center?: boolean;
 };
 
-const Modal: FC<Props> = ({ children, onClose }) => {
+const Modal: FC<Props> = ({ children, footer, onClose, className, center }) => {
   return (
-    <div className="Modal-container" onClick={onClose}>
-      <div className="Modal" onClick={(event) => event.stopPropagation()}>
-        {children}
+    <div
+      className={`Modal-container ${center ? "Modal-container--center" : ""}`}
+      onClick={onClose}
+    >
+      <div
+        className={`Modal ${className || ""}`}
+        onClick={(event) => event.stopPropagation()}
+      >
+        <div className="Modal-content">{children}</div>
+        {footer && <div className="Modal-footer">{footer}</div>}
       </div>
     </div>
   );
